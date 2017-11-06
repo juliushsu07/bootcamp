5387359a5fb446c811099df0fb6d97fa88979ebb
Julius Hsu <juliushsu07@gmail.com>
2017-11-06 18:41:09 +0000

    step 1 :got data chunk

--- w2d1/https_example.js
+++ w2d1/https_example.js
@@ -4,7 +4,18 @@ function getAndPrintHTMLChunks () {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
-
   /* Add your code here */
+  var https = require('https');
+
+  https.get(requestOptions, function(response){
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data){
+      console.log(data);
+    });
+  })
 
 }
+
+getAndPrintHTMLChunks ();
