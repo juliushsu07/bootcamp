aa76c26a14b379fcec8e79da8fe71b7818763c30
Julius Hsu <juliushsu07@gmail.com>
2017-11-06 20:22:22 +0000

    created callbacks for https example step 6

---
+++ w2d1/https_example3.js
@@ -0,0 +1,26 @@
+function getAndPrintHTML (options) {
+
+  var https = require('https');
+
+  https.get(options, function(response){
+
+    response.setEncoding('utf8');
+
+    var buffer = "";
+    response.on('data', function(data){
+      buffer += data;
+    })
+    response.on('end', function() {
+      console.log(buffer);
+    })
+
+  })
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML (requestOptions);
